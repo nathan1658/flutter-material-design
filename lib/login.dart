f540200b80c7abc97f01c910ae23afe52894ed6a
nathan1658 <>
2021-12-16 11:45:32 +0800

    - finished 101

--- lib/login.dart
+++ lib/login.dart
@@ -23,6 +23,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,11 +43,43 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             const SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration:
+                  const InputDecoration(filled: true, labelText: 'Username'),
+            ),
+            // spacer
+            const SizedBox(
+              height: 12,
+            ),
+            TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  labelText: 'Password',
+                ),
+                obscureText: true),
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
             // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
+            ButtonBar(
+              children: [
+                TextButton(
+                  child: Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('NEXT')),
+              ],
+            ),
           ],
         ),
       ),
